c1b70af243bd13486ae5536f6825ecc130584b0b
tannerfronk <tannerjace07@hotmail.com>
2020-01-29 21:16:19 -0700

    made hamburger view more even and removed junk JS

--- website/index.js
+++ website/index.js
@@ -5,7 +5,6 @@
 let menuText = document.getElementsByClassName('goAway')
 let heroPhone = document.getElementById('heroPhone');
 let hamburgerMenu = document.getElementById('hamburger');
-let x = window.matchMedia('@media only screen and (max-width: 700px)');
 
 
 console.log(menuText.length)
@@ -15,25 +14,12 @@ document.getElementById('hamburger').addEventListener('click', function(){
             heroPhone.style.marginTop = "0rem";
             menuText[i].style.visibility = "hidden";
        } else {
-        heroPhone.style.marginTop = "32rem";
+        heroPhone.style.marginTop = "31rem";
         menuText[i].style.visibility = "visible";
        }
     }
     console.log(window.innerWidth)
 })
-
-
-//function fixDesktopWindow(){
-//    if(!x.matches) {
-//        heroPhone.style.marginTop = "0";
-//    }
-//}
-//
-//function fixPhoneWindow(){
-//    if(x.matches){
-//        heroPhone.style.marginTop = "5rem";
-//    }
-//}
 
 function fixWindow(){
     if(window.innerWidth >= 700){
@@ -42,6 +28,6 @@ function fixWindow(){
         menuText[i].style.visibility = "visible";
         }
     } else if(window.innerWidth <= 699){
-        heroPhone.style.marginTop = "32rem";
+        heroPhone.style.marginTop = "31rem";
     }
 }
